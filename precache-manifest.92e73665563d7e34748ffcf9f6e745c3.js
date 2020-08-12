cec43ab6d95484a2f0e4d25be8ff6cea8bbfe5e4
heroesIV <omkar.alternative@gmail.com>
2020-08-13 01:07:12 +0530

    Updates

---
+++ precache-manifest.92e73665563d7e34748ffcf9f6e745c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bbf9adae49d0d93b6ca5fa5ec05dd3d",
+    "url": "/conway-game-of-life/index.html"
+  },
+  {
+    "revision": "56d3890e82c83eb4e1bb",
+    "url": "/conway-game-of-life/static/css/main.f2bed476.chunk.css"
+  },
+  {
+    "revision": "e5c60af5e352638960a5",
+    "url": "/conway-game-of-life/static/js/2.66322e02.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/conway-game-of-life/static/js/2.66322e02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56d3890e82c83eb4e1bb",
+    "url": "/conway-game-of-life/static/js/main.b2ae6ae9.chunk.js"
+  },
+  {
+    "revision": "3d2fdeb501c028b41f19",
+    "url": "/conway-game-of-life/static/js/runtime-main.2d9e8f3e.js"
+  }
+]);
